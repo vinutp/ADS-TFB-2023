088b691627f935e1496efb88774f3eb2df424e33
Irvin Chang <irvinchang17@gmail.com>
2025-11-06 23:50:46 +0800

    adding new file

---
+++ 25_10_16-Task1/task_1.c
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node
+{
+	string data;
+	Node* next_ptr;
+};
+int main()
+{
+	Node node1, node2, node3;
+	node1.data = "test1";
+	node2.data = "test2";
+	node3.data = "test3";
+
+	node1.next_ptr = &node2;
+	node2.next_ptr = &node3;
+	node3.next_ptr = &node1;
+
+	Node* current = &node1;
+
+	int n;
+	cout << "How many time do you want to traverse: ";
+	cin >> n;
+
+	for (int i = 0; i < n ; i++)
+	{
+		cout << current->data << " ";
+		current = current->next_ptr;
+	}
+	cout << endl;
+
+	return 0;
+}
